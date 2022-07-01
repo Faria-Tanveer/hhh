e0c2d905b872ae3d21365b038ad4dd5441506223
Faria Tanveer <55253715+Faria-Tanveer@users.noreply.github.com>
2022-07-01 07:05:05 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program that greets user
+            Console.WriteLine("Enter your name");
+            string a;
+            a = Console.ReadLine();
+            Console.WriteLine("Hello " + a);
+            Console.ReadLine();
+        }
+    }
+}
